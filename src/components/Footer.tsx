a637564666dd22bb12ab1140c6239c349af4fd3c
Mubashir-Abdullah <155892252+Mubashir-Abdullah@users.noreply.github.com>
2024-12-05 04:32:46 +0500

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-   <footer>
-<p>&copy; 2024 Watch World. All Rights Reserved.</p>
+    <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#333', color: 'white' }}>
+      <p>&copy; 2024 Watch World. All Rights Reserved.</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,18 +1,17 @@
-import { url } from 'inspector'
-import React from 'react'
+import React from 'react';
 
 const HomeContent = () => {
   return (
     <div>
-      <section className='hero' style={{backgroundImage:"url(image03.jpg)"}}>
+      <section className='hero' style={{ backgroundImage: "url(/image03.jpg)" }}>
         <div>
             <h1 className='fade-in'>Welcome to Watch World</h1>
-        <p> Your one-stop  shop for the best watches </p>
-        <button >Shope Now</button>
+            <p>Your one-stop shop for the best watches</p>
+            <button>Shope Now</button>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default HomeContent
+export default HomeContent;
